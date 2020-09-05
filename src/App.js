ef1e34484a38435b59da564404685eca1b292bdb
RJJ95 <ricardo.de.vries@hotmail.com>
2020-09-05 16:18:57 +0200

    continued on app file

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Pages
 
 // Config
+import { PATHNAMES } from "./config/pathnames";
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-        <Route path="/">
+        <Route path={PATHNAMES.HOME}>
           <Home />
+        </Route>
+        <Route path={PATHNAMES.REACT}>
+          <React />
+        </Route>
+        <Route path={PATHNAMES.VUE}>
+          <Vue />
+        </Route>
+        <Route path={PATHNAMES.ANGULAR}>
+          <Angular />
         </Route>
       </Switch>
     </Router>
