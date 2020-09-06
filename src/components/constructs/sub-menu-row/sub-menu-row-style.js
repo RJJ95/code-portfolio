afae180d94699b29d5793ece0c49eb0e3642fdc6
RJJ95 <ricardo.de.vries@hotmail.com>
2020-09-06 18:41:18 +0200

    added margin right to last child submenu items container

--- src/components/constructs/sub-menu-row/sub-menu-row-style.js
+++ src/components/constructs/sub-menu-row/sub-menu-row-style.js
@@ -4,16 +4,20 @@ import { Column } from "../../primitives/column";
 import { Row } from "../../primitives/row";
 
 export const Container = styled(Column)`
-    margin-bottom: 60px;
-    margin-left: 100px;
+  margin-bottom: 60px;
+  margin-left: 100px;
 `;
 
 export const Label = styled.label`
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 `;
 
 export const SubMenuItemsContainer = styled(Row)`
   display: flex;
   flex-wrap: nowrap;
   overflow-x: auto;
+
+  :last-child {
+    margin-right: 100px;
+  }
 `;
