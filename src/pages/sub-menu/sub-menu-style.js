e3540ae175bfba7749cae35ff1c09655e841f8f6
RJJ95 <ricardo.de.vries@hotmail.com>
2020-09-06 13:12:38 +0200

    created getlogo function

--- src/pages/sub-menu/sub-menu-style.js
+++ src/pages/sub-menu/sub-menu-style.js
@@ -1,15 +1,14 @@
 import styled from "styled-components";
 
 import { Column } from "../../components/primitives/column";
-import { ReactComponent as Logo } from "../../assets/images/react-logo.svg";
 
 export const Container = styled(Column)`
   width: 100%;
-`;
 
-export const ReactLogo = styled(Logo)`
-  width: 100px;
-  margin-left: 80px;
-  margin-top: 30px;
-  margin-bottom: 40px;
+  svg {
+    width: 100px;
+    margin-left: 80px;
+    margin-top: 30px;
+    margin-bottom: 40px;
+  }
 `;
--- src/pages/sub-menu/sub-menu-view.js
+++ src/pages/sub-menu/sub-menu-view.js
@@ -1,16 +1,37 @@
 import React from "react";
 
-import { Container, ReactLogo } from "./sub-menu-style";
+// Styles
+import { Container } from "./sub-menu-style";
 
+// Content
 import {
   reactSubMenuContent,
   vueSubMenuContent,
   angularSubMenuContent,
 } from "./content";
 
+// Components
 import SubMenuRow from "../../components/constructs/sub-menu-row";
+import { ReactComponent as ReactLogo } from "../../assets/images/react-logo.svg";
+import { ReactComponent as VueLogo } from "../../assets/images/vue-logo.svg";
+import { ReactComponent as AngularLogo } from "../../assets/images/angular-logo.svg";
 
 const SubMenuPage = ({ page }) => {
+  function getLogo() {
+    switch (page) {
+      case "react":
+        return <ReactLogo />;
+      case "vue":
+        return <VueLogo />;
+      case "angular":
+        return <AngularLogo />;
+      default:
+        return reactSubMenuContent.map((subMenuRow) => (
+          <SubMenuRow label={subMenuRow.label} items={subMenuRow.items} />
+        ));
+    }
+  }
+
   function getSubMenuContent() {
     switch (page) {
       case "react":
@@ -18,11 +39,11 @@ const SubMenuPage = ({ page }) => {
           <SubMenuRow label={subMenuRow.label} items={subMenuRow.items} />
         ));
       case "vue":
-        vueSubMenuContent.map((subMenuRow) => (
+        return vueSubMenuContent.map((subMenuRow) => (
           <SubMenuRow label={subMenuRow.label} items={subMenuRow.items} />
         ));
       case "angular":
-        angularSubMenuContent.map((subMenuRow) => (
+        return angularSubMenuContent.map((subMenuRow) => (
           <SubMenuRow label={subMenuRow.label} items={subMenuRow.items} />
         ));
       default:
@@ -34,7 +55,7 @@ const SubMenuPage = ({ page }) => {
 
   return (
     <Container>
-      <ReactLogo />
+      {getLogo()}
       {getSubMenuContent()}
     </Container>
   );
