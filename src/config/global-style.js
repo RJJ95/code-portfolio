55dcdf797321885c3568f4f6569ea4e26445f88b
RJJ95 <ricardo.de.vries@hotmail.com>
2020-09-06 20:44:11 +0200

    some cleanup

--- src/config/global-style.js
+++ src/config/global-style.js
@@ -1,10 +1,23 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
   label {
     color: ${(props) => props.theme.colors.lightGrey};
     font-weight: 600;
   }
+
+  pre {
+  margin: 0;
+}
 `;
 
 export default GlobalStyle;
